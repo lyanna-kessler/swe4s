6812dc8a68dbf6b37abc1fb626a48633fefdaa53
Lyanna Kessler <lyannakessler@gmail.com>
2020-09-08 18:21:27 -0600

    trying

---
+++ var.py
@@ -0,0 +1,23 @@
+import math
+import random
+#import numpy as np
+
+print(math.gcd, 1000, 2)
+print(math.log, 0.5)
+
+
+# def foo(first, second, third, fourth, fifth=100, sixth=11):
+#     positional arguments go first
+#     put named (default) arguments at the end of a function
+#     print(first, second, third, fourth, fifth, sixth)
+
+# foo(1,2, 'python hates types', 4, fifth=200)
+
+# a = 10
+# a = 'long string'
+# a = 1.1
+# b=0
+
+# if a > b:
+#     print(a, 'is greater than', b)
+
